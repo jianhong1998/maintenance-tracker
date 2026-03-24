5a058689ea485c7b8563cbed680c523415294d40
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2026-03-24 14:50:49 +0800

    feat: 010 - update frontend env var name

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    FRONTEND_FIREBASE_API_KEY: process.env.FRONTEND_FIREBASE_API_KEY,
+    FRONTEND_FIREBASE_AUTH_DOMAIN: process.env.FRONTEND_FIREBASE_AUTH_DOMAIN,
+    FRONTEND_FIREBASE_PROJECT_ID: process.env.FRONTEND_FIREBASE_PROJECT_ID,
+  },
 };
 
 export default nextConfig;
--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -2,9 +2,9 @@ import { getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.FRONTEND_FIREBASE_API_KEY,
+  authDomain: process.env.FRONTEND_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FRONTEND_FIREBASE_PROJECT_ID,
 };
 
 const app =
