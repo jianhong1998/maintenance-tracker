129682d9e82a24cfc68f568b94bb8e597f1f7c92
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2026-03-25 11:41:00 +0800

    feat: 011 - simplify createWrapper to derive from createWrapperWithClient

--- frontend/src/hooks/queries/test-utils.ts
+++ frontend/src/hooks/queries/test-utils.ts
@@ -1,20 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
-export const createWrapper = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-    },
-  });
-  const Wrapper = ({ children }: { children: React.ReactNode }) =>
-    React.createElement(QueryClientProvider, { client: queryClient }, children);
-  Wrapper.displayName = 'TestQueryClientWrapper';
-  return Wrapper;
-};
-
 export const createWrapperWithClient = () => {
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -28,3 +14,5 @@ export const createWrapperWithClient = () => {
   wrapper.displayName = 'TestQueryClientWrapper';
   return { wrapper, queryClient };
 };
+
+export const createWrapper = () => createWrapperWithClient().wrapper;
